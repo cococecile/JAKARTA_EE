374d705648d92469fde173b157a7edc5eb8f4dc4
Céccile <simoncecilepro@outlook.fr>
2021-11-04 16:27:31 +0100

    ControllerArticle

--- pharmacie/src/main/java/poei/presentation/controller/ArticleController.java
+++ pharmacie/src/main/java/poei/presentation/controller/ArticleController.java
@@ -1,56 +1,23 @@
 package poei.presentation.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
-
-import poei.presentation.bean.ArticleDto;
-import poei.service.IArticleService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ArticleController {
-	
-	@Autowired
-    private IArticleService service;
 
-    @GetMapping
-    public String showAllArticles(Model model) {
-        model.addAttribute("articlesList", service.getAllArticle());
-        return "articlesList";
+    @RequestMapping(path="/", method=RequestMethod.GET)
+    public String getHome() {
+        // ...
+        return "index";
     }
 
-    @GetMapping("/articleCreate")
-    public String showArticleCreationForm(Model model) {
-        model.addAttribute("articleList", new ArticleDto());
-        return "articleCreate";
-    }
-
-    @PostMapping(value = "/articleCreate", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public String addNewArticle(@ModelAttribute ArticleDto articledto, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "articleDétail";
-        }
-        service.create(articledto);
-        model.addAttribute("articlesList", service.getAllArticle());
-        return "articleList";
-    }
-
-    @PostMapping("/{id}/articleUpdate")
-    public String updateArticle(@PathVariable int id, @ModelAttribute ArticleDto articleDto, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "articleDétail";
-        }
-        model.addAttribute("articleDétail", service.getAllArticle());
-        return "articlesList";
+    @RequestMapping(path="/item", method=RequestMethod.POST)
+    public String addItem() {
+        // ...
+        return "item";
     }
 
 }
-  
 
