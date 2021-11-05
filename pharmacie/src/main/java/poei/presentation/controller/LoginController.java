35ef78eb1ac869ef76a31ed69a79b4a1c40c28d3
Charline8380 <charline.abuwe@gmail.com>
2021-11-05 15:52:41 +0100

    resolution de conflit

--- pharmacie/src/main/java/poei/presentation/controller/LoginController.java
+++ pharmacie/src/main/java/poei/presentation/controller/LoginController.java
@@ -9,8 +9,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import poei.presentation.bean.UserDto;
