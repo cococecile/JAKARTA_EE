f82839fe2581a0950456dc881354cccbd16c588a
Céccile <simoncecilepro@outlook.fr>
2021-11-05 18:05:00 +0100

    JSPController

--- pharmacie/src/main/java/poei/presentation/controller/ArticleController.java
+++ pharmacie/src/main/java/poei/presentation/controller/ArticleController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import poei.presentation.bean.ArticleDto;
+import poei.service.IUserService;
 import poei.service.imp.ArticleService;
 
 @Controller
@@ -15,13 +18,31 @@ public class ArticleController {
 	
 	  @Autowired
 	    ArticleService articleService;
+	  
+	  @Autowired
+	    private IUserService userService;
 
-  
+	  
+	  
+	  
+	  
+	  
+	  
+	  public ModelAndView article() {
+		  return new ModelAndView("articleCreate");
+	  }
+	  
+	  
+	@RequestMapping("/articlesList")
+	public String showListArticlesPage(Model model) {
+	    model.addAttribute("articles", articleService.getAllArticle());
+	    return "articlesList";
+	}
 
     @RequestMapping("/articleCreate")
     public String showCreateArticlePage(Model model) {
         model.addAttribute("command", new ArticleDto());
-        return "articlecreate";
+        return "articleCreate";
     }
     
     @RequestMapping(value = "/articleCreate", method = RequestMethod.POST)
@@ -30,6 +51,21 @@ public class ArticleController {
         return "redirect:/articleDetail";
     }
 
-    
+
+    @RequestMapping(value = "/articleUpdate/{id}")
+    public String showUpdateArticlePage(@PathVariable int id, Model model) {
+        model.addAttribute("id", id);
+        model.addAttribute("command", articleService.getById(id));
+        return "articleUpdate";
+    }
+
+    @RequestMapping(value = "/articleUpdate/{id}", method = RequestMethod.POST)
+    public String updateArticle(@PathVariable int id, @ModelAttribute("article") ArticleDto article) {
+        articleService.updateArticle(article, id);
+        return "redirect:/articleDétail";
+    }
+ 
 }
+    
+    
 
